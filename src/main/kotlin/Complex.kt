f9066cb4da9e1b84bf5d3d6ead04a3aec5639ec3
Jörg Vollmer <kotlincook@gmx.de>
2020-02-09 02:04:56 +0100

    toString(), i, extension functions, etc.

--- src/main/kotlin/Complex.kt
+++ src/main/kotlin/Complex.kt
@@ -1,11 +1,23 @@
+package org.kotlincook.math
+
+import java.lang.NumberFormatException
+import java.util.*
 import kotlin.math.*
 
-fun String.toComplex(): Complex {
-    return Complex(0.0, 0.0) // FIXME
+const val π = PI
+
+val i = Complex(0.0, 1.0)
+
+val Double.i: Complex
+    get() = Complex(0.0, this)
+
+data class Polar(val mod: Double, val arg: Double) {
+    fun toComplex() = Complex(mod * cos(arg), mod * sin(arg))
 }
 
 data class Complex(val re: Double, val im: Double = 0.0) {
     constructor(z: Complex) : this(z.re, z.im)
+    constructor(polarCoord: Polar) : this(polarCoord.toComplex())
     constructor(str: String) : this(str.toComplex())
 
     val arg: Double
@@ -31,15 +43,35 @@ data class Complex(val re: Double, val im: Double = 0.0) {
 
     operator fun div(c: Double): Complex = Complex(re / c, im / c)
 
-    override fun toString() = asString()
+    fun toPolar(): Polar = Polar(mod(this), arg(this))
 
-    fun asString(): String = this.re.toString() + (
+    override fun toString(): String = this.re.toString() + (
             when {
                 this.im > 0.0 -> "+" + this.im + "i"
                 this.im < 0.0 -> "" + this.im + "i"
                 else -> ""
             })
+}
 
+fun String.toComplex(): Complex {
+    val parts = StringTokenizer(this, "+-", true)
+            .toList().map { it.toString() }
+    return when (parts.size) {
+        0 -> throw NumberFormatException("empty String")
+        1 -> if (parts[0].endsWith("i")) {
+                Complex(0.0, parts[0].removeSuffix("i").toDouble())
+             } else {
+                Complex(parts[0].toDouble())
+             }
+        2 -> if (parts[1].endsWith("i")) {
+                Complex(0.0, (parts[0] + parts[1].removeSuffix("i")).toDouble())
+             } else {
+                Complex((parts[0] + parts[1]).toDouble())
+             }
+        3 -> Complex(parts[0].toDouble(), (parts[1] + parts[2].removeSuffix("i")).toDouble())
+        4 -> Complex((parts[0] + parts[1]).toDouble(), (parts[2] + parts[3].removeSuffix("i")).toDouble())
+        else -> throw NumberFormatException("For input string: \"$this\"")
+    }
 }
 
 fun exp(z: Complex): Complex {
@@ -62,10 +94,28 @@ fun mod(z: Complex) = sqrt(z.re * z.re + z.im * z.im)
 fun arg(z: Complex): Double {
     val (x, y) = z
     return when {
-        x != 0.0 -> atan(y / x)
-        y == 0.0 -> 0.0
-        y > 0.0 -> PI
-        else -> -PI
+        x > 0.0 -> atan(y / x)
+        x < 0.0 && y >= 0.0 -> atan(y / x) + PI
+        x < 0.0 && y < 0.0 -> atan(y / x) - PI
+        x == 0.0 && y > 0.0 -> PI / 2
+        x == 0.0 && y < 0.0 -> -PI / 2
+        else -> 0.0
     }
 }
 
+operator fun Double.plus(c: Complex): Complex {
+    return Complex(this + c.re, c.im)
+}
+
+operator fun Double.minus(c: Complex): Complex {
+    return Complex(this - c.re, -c.im)
+}
+
+operator fun Double.times(c: Complex): Complex {
+    return Complex(this * c.re, this * c.im)
+}
+
+operator fun Double.div(c: Complex): Complex {
+    val d = c.re * c.re + c.im * c.im
+    return Complex((this * c.re) / d, (-this * c.im) / d)
+}
---
+++ src/test/kotlin/ComplexTest.kt
@@ -0,0 +1,47 @@
+package org.kotlincook.math
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class ComplexTest {
+
+    val eps = 1E-10
+
+    @ParameterizedTest()
+    @CsvSource(value =
+    ["2.0+3.0i;2.0;3.0", "-2.0+3.0i;-2.0;3.0", "2.0-3.0i;2.0;-3.0", "-2.0-3.0i;-2.0;-3.0",
+        "2.0;2.0;0.0", "-2.0;-2.0;0.0", "2.0i;0.0;2.0", "-2.0i;0.0;-2.0"
+    ], delimiter = ';')
+    fun testParsing(input: String, expRe: Double, expIm: Double) {
+        assertEquals(Complex(input), Complex(expRe, expIm))
+    }
+
+
+    @Test
+    fun testToPolarToComplex() {
+        val z = 2.0 + 3.0.i
+        assertEquals(z, z.toPolar().toComplex())
+    }
+
+    @Test
+    fun testPolar() {
+        assertTrue(mod(i - exp((π / 2).i)) < eps)
+    }
+
+    @Test
+    fun testEulerEquation() {
+        val z = "2.0+3.0i".toComplex()
+        assertTrue(mod(cos(z) + i * sin(z) - exp(i * z)) < eps)
+    }
+
+    @Test
+    fun testMod() {
+        val z = "2.0+3.0i".toComplex()
+        val r = mod(z)
+        assertTrue(mod(!z*z - r*r) < eps)
+    }
+
+}
