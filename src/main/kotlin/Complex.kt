2977535957b065b1522e07c9d11df7db87728882
Jörg Vollmer <kotlincook@gmx.de>
2020-02-09 22:55:42 +0100

    New design with emphasis on reusability

--- src/main/kotlin/Complex.kt
+++ src/main/kotlin/Complex.kt
@@ -4,49 +4,38 @@ import java.lang.NumberFormatException
 import java.util.*
 import kotlin.math.*
 
-val Double.i: Complex
-    get() = Complex(0.0, this)
-
-data class Polar(val mod: Double, val arg: Double) {
-    fun toComplex() = Complex(mod * cos(arg), mod * sin(arg))
-}
-
-const val π = PI
-val i = Complex(0.0, 1.0)
-val zero = Complex(0.0, 0.0)
-val one = Complex(1.0, 0.0)
-
-data class Complex(val re: Double, val im: Double = 0.0) {
-    constructor(z: Complex) : this(z.re, z.im)
-    constructor(polarCoord: Polar) : this(polarCoord.toComplex())
-    constructor(str: String) : this(str.toComplex())
-
+interface IComplex {
+    val re: Double
+    val im: Double
     val arg: Double
         get() {
-            return arg(this)
+            return when {
+                re > 0.0 -> atan(im / re)
+                re < 0.0 && im >= 0.0 -> atan(im / re) + PI
+                re < 0.0 && im < 0.0 -> atan(im / re) - PI
+                re == 0.0 && im > 0.0 -> PI / 2
+                re == 0.0 && im < 0.0 -> -PI / 2
+                else -> 0.0
+            }
         }
     val mod: Double
         get() {
-            return mod(this)
+            return sqrt(re * re + im * im)
         }
-
-    operator fun not(): Complex = conj(this)
-    operator fun plus(z: Complex): Complex = Complex(re + z.re, im + z.im)
-    operator fun minus(z: Complex): Complex = Complex(re - z.re, im - z.im)
-    operator fun plus(c: Double): Complex = Complex(re + c, im)
-    operator fun minus(c: Double): Complex = Complex(re - c, im)
-    operator fun times(z: Complex): Complex = Complex(re * z.re - im * z.im, im * z.re + re * z.im)
-    operator fun times(k: Double): Complex = Complex(re * k, im * k)
-    operator fun div(c: Complex): Complex {
+    operator fun not(): IComplex = conj()
+    operator fun plus(z: IComplex): IComplex = Complex(re + z.re, im + z.im)
+    operator fun minus(z: IComplex): IComplex = Complex(re - z.re, im - z.im)
+    operator fun plus(c: Double): IComplex = Complex(re + c, im)
+    operator fun minus(c: Double): IComplex = Complex(re - c, im)
+    operator fun times(z: IComplex): IComplex = Complex(re * z.re - im * z.im, im * z.re + re * z.im)
+    operator fun times(k: Double): IComplex = Complex(re * k, im * k)
+    operator fun div(c: IComplex): IComplex {
         val d = c.re * c.re + c.im * c.im
         return Complex((re * c.re + im * c.im) / d, (im * c.re - re * c.im) / d)
     }
-
-    operator fun div(c: Double): Complex = Complex(re / c, im / c)
-
-    fun toPolar(): Polar = Polar(mod(this), arg(this))
-
-    override fun toString(): String = this.re.toString() + (
+    operator fun div(c: Double): IComplex = Complex(re / c, im / c)
+    fun conj() = Complex(re, -im)
+    fun asString(): String = this.re.toString() + (
             when {
                 this.im > 0.0 -> "+" + this.im + "i"
                 this.im < 0.0 -> "" + this.im + "i"
@@ -54,69 +43,80 @@ data class Complex(val re: Double, val im: Double = 0.0) {
             })
 }
 
-fun String.toComplex(): Complex {
+val Double.I: IComplex
+    get() = Complex(0.0, this)
+
+operator fun Double.plus(c: IComplex): IComplex {
+    return Complex(this + c.re, c.im)
+}
+
+operator fun Double.minus(c: IComplex): IComplex {
+    return Complex(this - c.re, -c.im)
+}
+
+operator fun Double.times(c: IComplex): IComplex {
+    return Complex(this * c.re, this * c.im)
+}
+
+operator fun Double.div(c: IComplex): IComplex {
+    val d = c.re * c.re + c.im * c.im
+    return Complex((this * c.re) / d, (-this * c.im) / d)
+}
+
+fun String.toComplex(): IComplex {
     val parts = StringTokenizer(this, "+-", true)
             .toList().map { it.toString() }
     return when (parts.size) {
         0 -> throw NumberFormatException("empty String")
         1 -> if (parts[0].endsWith("i")) {
-                Complex(0.0, parts[0].removeSuffix("i").toDouble())
-             } else {
-                Complex(parts[0].toDouble())
-             }
+            Complex(0.0, parts[0].removeSuffix("i").toDouble())
+        } else {
+            Complex(parts[0].toDouble(), 0.0)
+        }
         2 -> if (parts[1].endsWith("i")) {
-                Complex(0.0, (parts[0] + parts[1].removeSuffix("i")).toDouble())
-             } else {
-                Complex((parts[0] + parts[1]).toDouble())
-             }
+            Complex(0.0, (parts[0] + parts[1].removeSuffix("i")).toDouble())
+        } else {
+            Complex((parts[0] + parts[1]).toDouble(), 0.0)
+        }
         3 -> Complex(parts[0].toDouble(), (parts[1] + parts[2].removeSuffix("i")).toDouble())
         4 -> Complex((parts[0] + parts[1]).toDouble(), (parts[2] + parts[3].removeSuffix("i")).toDouble())
         else -> throw NumberFormatException("For input string: \"$this\"")
     }
 }
 
-fun exp(z: Complex): Complex {
+var exp: (IComplex) -> IComplex = { z ->
     val r: Double = exp(z.re)
-    return Complex(r * cos(z.im), r * sin(z.im))
+    Complex(r * cos(z.im), r * sin(z.im))
 }
 
-fun sin(z: Complex): Complex {
-    val (x, y) = z
-    return Complex(sin(x) * cosh(y), cos(x) * sinh(y))
+var sin: (IComplex) -> IComplex = { z ->
+    Complex(sin(z.re) * cosh(z.im), cos(z.re) * sinh(z.im))
 }
 
-fun cos(z: Complex): Complex {
-    val (x, y) = z
-    return Complex(cos(x) * cosh(y), -sin(x) * sinh(y))
+var cos: (IComplex) -> IComplex = { z ->
+    Complex(cos(z.re) * cosh(z.im), -sin(z.re) * sinh(z.im))
 }
 
-fun conj(z: Complex) = Complex(z.re, -z.im)
-fun mod(z: Complex) = sqrt(z.re * z.re + z.im * z.im)
-fun arg(z: Complex): Double {
-    val (x, y) = z
-    return when {
-        x > 0.0 -> atan(y / x)
-        x < 0.0 && y >= 0.0 -> atan(y / x) + PI
-        x < 0.0 && y < 0.0 -> atan(y / x) - PI
-        x == 0.0 && y > 0.0 -> PI / 2
-        x == 0.0 && y < 0.0 -> -PI / 2
-        else -> 0.0
-    }
+
+var Complex: (re:Double, im: Double) -> IComplex = { re:Double, im:Double -> ComplexImpl(re, im) }
+val I = Complex(0.0, 1.0)
+val ZERO = Complex(0.0, 0.0)
+val ONE = Complex(1.0, 0.0)
+
+data class Polar(val mod: Double, val arg: Double) {
+    fun toComplex() = Complex(mod * cos(arg), mod * sin(arg))
+}
+data class ComplexImpl(override val re: Double, override val im: Double = 0.0): IComplex {
+    constructor(z: IComplex) : this(z.re, z.im)
+    constructor(polarCoord: Polar) : this(polarCoord.toComplex())
+    constructor(str: String) : this(str.toComplex())
+
+    fun toPolar(): Polar = Polar(mod, arg)
+    override fun toString(): String = asString()
 }
 
-operator fun Double.plus(c: Complex): Complex {
-    return Complex(this + c.re, c.im)
-}
 
-operator fun Double.minus(c: Complex): Complex {
-    return Complex(this - c.re, -c.im)
-}
 
-operator fun Double.times(c: Complex): Complex {
-    return Complex(this * c.re, this * c.im)
-}
-
-operator fun Double.div(c: Complex): Complex {
-    val d = c.re * c.re + c.im * c.im
-    return Complex((this * c.re) / d, (-this * c.im) / d)
+fun main(args: Array<String>) {
+    println(exp(PI*I))
 }
--- src/test/kotlin/ComplexTest.kt
+++ src/test/kotlin/ComplexTest.kt
@@ -3,6 +3,7 @@ package org.kotlincook.math
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
+import java.lang.Math.PI
 import kotlin.test.assertEquals
 import kotlin.test.assertTrue
 
@@ -16,39 +17,39 @@ class ComplexTest {
         "2.0;2.0;0.0", "-2.0;-2.0;0.0", "2.0i;0.0;2.0", "-2.0i;0.0;-2.0"
     ], delimiter = ';')
     fun testParsing(input: String, expRe: Double, expIm: Double) {
-        assertEquals(Complex(input), Complex(expRe, expIm))
+        assertEquals(input.toComplex(), Complex(expRe, expIm))
     }
 
 
     @Test
     fun testToPolarToComplex() {
-        val z = 2.0 + 3.0.i
+        val z = (2.0 + 3.0.I) as ComplexImpl
         assertEquals(z, z.toPolar().toComplex())
     }
 
     @Test
     fun testPolar() {
-        assertTrue(mod(i - exp((π / 2).i)) < eps)
+        assertTrue((I - exp((PI / 2).I)).mod < eps)
     }
 
     @Test
     fun testEulerEquation() {
         val z = "2.0+3.0i".toComplex()
-        assertTrue(mod(cos(z) + i * sin(z) - exp(i * z)) < eps)
+        assertTrue((cos(z) + I * sin(z) - exp(I * z)).mod < eps)
     }
 
     @Test
     fun testMod() {
         val z = "2.0+3.0i".toComplex()
-        val r = mod(z)
-        assertTrue(mod(!z*z - r*r) < eps)
+        val r = z.mod
+        assertTrue((!z*z - r*r).mod < eps)
     }
 
     @Test
     fun testConstants() {
         val z = "2.0+3.0i".toComplex()
-        assertEquals(z * zero, zero)
-        assertEquals(z * one, z)
+        assertEquals(z * ZERO, ZERO)
+        assertEquals(z * ONE, z)
     }
 
 }
